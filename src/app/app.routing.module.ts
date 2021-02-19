dcda422d02eeda1b1c7247421dbf1e7cdb7c47e4
Klécio Silva <kleciosilvajp@gmail.com>
2021-02-18 22:14:17 -0300

    Resolver

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,11 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { PhotoListResolver } from './photos/photo-list/photo-list.ressolver';
 
 const routes: Routes = [
-  { path: 'user/:userName', component: PhotoListComponent },
+  { path: 'user/:userName', component: PhotoListComponent, resolve: { photos: PhotoListResolver} },
   { path: 'p/add', component: PhotoFormComponent },
-  { path: '**', component: NotFoundComponent }
+  { path: '**', component: NotFoundComponent },
 ]
 
 @NgModule({
